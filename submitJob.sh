91e4e8f27d05f8081fff8a0694dcdd960edcaa63
saman <saman.noorzadeh@surgar-surgery.com>
2024-05-04 23:00:47 +0200

    adapted to my dataest

--- submitJob.sh
+++ submitJob.sh
@@ -2,7 +2,7 @@
 
 ##Resource Request
 
-#SBATCH --job-name incisionDeeplab
+#SBATCH --job-name MaskFall
 #SBATCH --output=stdout
 #SBATCH --error=sdterr
 #SBATCH --ntasks=1  ## number of tasks (analyses) to run
@@ -10,7 +10,7 @@
 #SBATCH --cpus-per-task=3 # number of gpus per task
 
 #SBATCH --mem-per-gpu=20000M # Memory allocated per gpu
-#SBATCH --time=0-12:00:00  ## time for analysis (day-hour:min:sec)
+#SBATCH --time=0-24:00:00  ## time for analysis (day-hour:min:sec)
 
 ## Run the script
 srun python train.py --epochs 300 --batch 10 --scheduler
